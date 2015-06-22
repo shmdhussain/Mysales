65d8f5fa34f60ac8ee7430c1735ab4753dfa6454
Mohamed Hussain S H <shmdhussain12@gmail.com>
2015-06-22 13:04:52 +0530

    added chart changes

--- js/Controllers/ctrl.js
+++ js/Controllers/ctrl.js
@@ -14,7 +14,7 @@ myApp.controller("loginCtrl", ['$scope','AuthenticationService','$location','use
 	console.log("userdataPromise data: "+userDataObj);
 }]);
 
-myApp.controller("homeCtrl", ['$scope','AuthenticationService','userdata','$location','$modal','$log', function($scope, AuthenticationService,userdata,$location,$modal,$log) {
+myApp.controller("homeCtrl", ['$scope','AuthenticationService','userdata','$location','$modal','$log','$timeout', function($scope, AuthenticationService,userdata,$location,$modal,$log,$timeout) {
 	console.log("homeCtrl");
 	console.log("userdata: "+userdata.sessionId);
 	angular.element("html").removeClass("coverBg");
@@ -41,6 +41,53 @@ myApp.controller("homeCtrl", ['$scope','AuthenticationService','userdata','$loca
 		AuthenticationService.logout();
 	};
 	
+	
+	//openchart
+	
+	$scope.openchart = function (modalURL,classforModal) {
+	
+		jQuery("body").css({"position":"fixed","width":"100%","height":"100%"});
+		jQuery("html").css({"position":"fixed","overflow-y":"hidden"});
+		jQuery("html").addClass("mychart");
+		$timeout(function(){
+				console.log('jQuery(".mychart .modal-dialog").length: '+jQuery(".mychartmodal .modal-dialog").length);
+				jQuery(".mychart .modal-dialog").css({"width":"100%","margin":"0px"});
+				jQuery(".mychart .modal-content").css({"height":"100%","border":"none","border-radius":"0px","box-shadow":"none","-webkit-box-shadow":"none","-moz-box-shadow":"none"});
+				fnModalScrollfix();
+		},100);
+		
+		var modalInstance = $modal.open({
+		  animation: true,
+		  templateUrl: 'partials/'+modalURL,
+		  windowClass:classforModal
+		});
+		modalInstance.opened.then(function (result) {
+			
+			
+			
+		  
+		}, function () {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
+		
+		
+		modalInstance.result.then(function (result) {
+		 
+		  modalreset();
+		  
+		}, function () {
+			modalreset();
+		  $log.info('Modal dismissed at: ' + new Date());
+		});
+	};
+	
+	function modalreset(){
+		// jQuery("body").css({"position":"static","width":"","height":""});
+		// jQuery("html").css({"position":"static","overflow-y":""});
+		jQuery("body").removeAttr("style");
+		jQuery("html").removeAttr("style");
+		jQuery("html").removeClass("mychart");
+	}
 	
 }]);
 
--- js/app.js
+++ js/app.js
@@ -38,4 +38,64 @@
 		$rootScope.message = {'toshow':false,'message':'','class':''}; 
 	});
 	
-	
+	 // MODAL ADJUSTMENT
+	fnModalScrollfix = (function() {
+		var $=jQuery;
+		var $this = $('.modal.in');
+		var $headHeight = $('.modal.in  .modal-header').outerHeight();
+		var $footHeight = $('.modal.in  .modal-footer').outerHeight(); 
+		var $modalHeight = window.innerHeight; //$(window).height();
+		
+		if($headHeight == null || $headHeight == -1){$headHeight = '0'}
+		if($footHeight == null || $footHeight == -1){$footHeight = '0'}
+		
+		var $bodyHeight = $modalHeight - (parseInt($headHeight) + parseInt($footHeight)) + 'px';
+		$('.modal.in  .modal-body').css({
+			'height':$bodyHeight
+		});
+		console.log("$$headHeight: "+$headHeight);
+		console.log("$footHeight: "+$footHeight);
+		console.log("$bodyHeight: "+$bodyHeight);
+		console.log("$modalHeight: "+$modalHeight);
+		if( 1){ // if portrait mobile
+			$('.modal.in').css({'overflow-y':'hidden', '-webkit-overflow-scrolling':'auto'});
+			$('.modal.in').find('.modal-header').css({
+				'-webkit-overflow-scrolling':'auto'
+			});
+			$('.modal.in').find('.modal-body').css({
+				'height':$bodyHeight,
+				'overflow-y':'scroll',
+				'padding-bottom':'15px',
+				'-webkit-overflow-scrolling':'touch'
+			});
+			$('.modal.in').find('.modal-footer').css({
+				'box-sizing':'border-box',
+				'margin':'0',
+				'position':'static',
+				'width':'100%',
+				'-webkit-overflow-scrolling':'auto'
+			});            
+		}
+		else{ 
+			$('.modal.in').css({'overflow-y':'scroll', '-webkit-overflow-scrolling':'touch'});
+			$('.modal.in').find('.modal-header').css({
+				'-webkit-overflow-scrolling':'auto'
+			});
+			$('.modal.in').find('.modal-body').css({
+				'height':'auto',
+				'overflow-y':'hidden',
+				'padding-bottom':'15px',
+				'-webkit-overflow-scrolling':'auto'
+			});
+			$('.modal.in').find('.modal-footer').css({
+				'box-sizing':'content-box',
+				'margin':'0',
+				'position':'static',
+				'width':'auto',
+				'-webkit-overflow-scrolling':'auto'
+			});
+		}
+   });
+jQuery(window).on("resize", function() {
+   fnModalScrollfix();
+});
